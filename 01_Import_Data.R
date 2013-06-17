d76dd068b362c0c06870a764465da30e1ec35b45
Dr. Guido Moeser <guido.moeser@gmail.com>
2013-06-17 16:10:55 +0200

    Further syntax

--- 01_Import_Data.R
+++ 01_Import_Data.R
@@ -1,4 +1,8 @@
 ## Load and preprocess data from single studies
+## we will automate as far as possible later on - this is work in progress
+## Started today (June 17th, 2013)
+## Author: Guido Moeser
+
 
 ## Import from Excel-Tables
 
@@ -24,10 +28,104 @@ library(rJava)
 #install.packages("xlsx", dependencies = TRUE)
 library(xlsx)
 
+
+##### F I R S T    D A T A S E T ##
+
 ## Read first sheet in table
 sheet_d1 <- read.xlsx("Daten_Final_Procedure_05_April_2006.xls", sheetName = "d1", rowIndex = c(2:11), colIndex = c(2:12))
+# rowIndex is not working correct...
 
-# rowIndex is not working probably...
+## collect all correlations
+cor_d1 <- sheet_d1[c(1:10),]
+cor_d1
+
+## Change row and column names
+colnames(cor_d1) <- c("VAR_", "PROB", "AC", "RESP", "SNORM", "GUILT", "PBC", "ATT", "MNORM", "INTENT", "BEHAV")
+colnames(cor_d1)
+cor_d1
+
+## Change Names in VAR_
+cor_d1$VAR_ <- c("PROB", "AC", "RESP", "SNORM", "GUILT", "PBC", "ATT", "MNORM", "INTENT", "BEHAV")
+cor_d1
+
+
+## Collect corresponding N
+N_d1 <- sheet_d1[c(11:20),]
+N_d1
+
+## Change row and column names
+colnames(N_d1) <- c("VAR_", "PROB", "AC", "RESP", "SNORM", "GUILT", "PBC", "ATT", "MNORM", "INTENT", "BEHAV")
+colnames(N_d1)
+N_d1
+
+## Change Names in VAR_
+N_d1$VAR_ <- c("PROB", "AC", "RESP", "SNORM", "GUILT", "PBC", "ATT", "MNORM", "INTENT", "BEHAV")
+N_d1
+
+## Clean up
+rm(sheet_d1)
+
+
+
+
+
+##### S E C O N D    D A T A S E T ##
+
+## Read first sheet in table
+sheet_d2 <- read.xlsx("Daten_Final_Procedure_05_April_2006.xls", sheetName = "d2", rowIndex = c(2:11), colIndex = c(2:12))
+# rowIndex is not working correct...
+
+## collect all correlations
+cor_d2 <- sheet_d2[c(1:10),]
+cor_d2
+
+## Change row and column names
+colnames(cor_d2) <- c("VAR_", "PROB", "AC", "RESP", "SNORM", "GUILT", "PBC", "ATT", "MNORM", "INTENT", "BEHAV")
+colnames(cor_d2)
+cor_d2
+
+## Change Names in VAR_
+cor_d2$VAR_ <- c("PROB", "AC", "RESP", "SNORM", "GUILT", "PBC", "ATT", "MNORM", "INTENT", "BEHAV")
+cor_d2
+
+
+## Collect corresponding N
+N_d2 <- sheet_d2[c(11:20),]
+N_d2
+
+## Change row and column names
+colnames(N_d2) <- c("VAR_", "PROB", "AC", "RESP", "SNORM", "GUILT", "PBC", "ATT", "MNORM", "INTENT", "BEHAV")
+colnames(N_d2)
+N_d2
+
+## Change Names in VAR_
+N_d2$VAR_ <- c("PROB", "AC", "RESP", "SNORM", "GUILT", "PBC", "ATT", "MNORM", "INTENT", "BEHAV")
+N_d2
+
+## Clean up
+rm(sheet_d2)
+
+
+
+
+
+##### F I R S T    M E T A - A N A L Y S I S ##
+## We will conduct our first meta-analysis now  
+## Classical approach - pooling correlation after correlation
+
+
+## Install and load packages
+#install.packages("metafor", dependencies = TRUE)
+library(metafor)
+# library meta is helpful in drawing Forest-Plots
+install.packages("meta", dependencies = TRUE)
+library(meta)
+
+
+## Extract necessary information for first meta-analysis
+## PROB <> AC
+
+
 
 
 
